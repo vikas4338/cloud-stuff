95369f0c01bf87b767a6d0ccd68e3ef8dd92a4d6
vikas4338 <vikas4338@gmail.com>
2023-12-06 20:57:39 -0600

    Implemented lambda functions for processing put and copy events and sns event filtering

--- cloud/aws/Projects/event-based-architecture/sns.tf
+++ cloud/aws/Projects/event-based-architecture/sns.tf
@@ -3,12 +3,15 @@ resource "aws_sns_topic" "sns_topic_for_s3_upload_updates" {
   name = "s3-event-notification-topic"
 }
 
+// Get the AWS account information
+data "aws_caller_identity" "current" {}
+
 // SNS policy to allow 
 // - S3 to push notification to S3
 // - SQS to subscibe the topic 
 resource "aws_sns_topic_policy" "default" {
-  arn    = aws_sns_topic.sns_topic_for_s3_upload_updates.arn
-  
+  arn = aws_sns_topic.sns_topic_for_s3_upload_updates.arn
+
   policy = <<EOF
 {
   "Version": "2008-10-17",
@@ -24,7 +27,7 @@ resource "aws_sns_topic_policy" "default" {
       "Resource": "${aws_sns_topic.sns_topic_for_s3_upload_updates.arn}",
       "Condition": {
         "StringEquals": {
-          "aws:SourceAccount": "604553012726"
+          "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
         },
         "ArnLike": {
           "aws:SourceArn": "${aws_s3_bucket.bucket_as_event_source.arn}"
@@ -41,7 +44,10 @@ resource "aws_sns_topic_policy" "default" {
       "Resource": "${aws_sns_topic.sns_topic_for_s3_upload_updates.arn}",
       "Condition": {
         "ArnEquals": {
-          "aws:SourceArn": "${aws_sqs_queue.sqs_queue_for_s3_updates.arn}"
+          "aws:SourceArn": [
+            "${aws_sqs_queue.queue_for_processing_put_events_on_s3.arn}",
+            "${aws_sqs_queue.queue_for_processing_copy_events_on_s3.arn}"
+          ]
         }
       }
     }
@@ -50,10 +56,22 @@ resource "aws_sns_topic_policy" "default" {
 EOF
 }
 
-
-// SQS Subscription to the SNS topic
-resource "aws_sns_topic_subscription" "s3_updates_subscription" {
+// SQS (for PUT events) Subscription to the SNS topic
+resource "aws_sns_topic_subscription" "s3_updates_subscription_put_events" {
   topic_arn = aws_sns_topic.sns_topic_for_s3_upload_updates.arn
   protocol  = "sqs"
-  endpoint  = aws_sqs_queue.sqs_queue_for_s3_updates.arn
+  endpoint  = aws_sqs_queue.queue_for_processing_put_events_on_s3.arn
+  filter_policy_scope = "MessageBody"
+  filter_policy = "{\"Records\":{\"eventName\":[\"ObjectCreated:Put\"]}}"
+  raw_message_delivery = true
+}
+
+// SQS (for COPY events) Subscription to the SNS topic
+resource "aws_sns_topic_subscription" "s3_updates_subscription_copy_events" {
+  topic_arn = aws_sns_topic.sns_topic_for_s3_upload_updates.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.queue_for_processing_copy_events_on_s3.arn
+  filter_policy_scope = "MessageBody"
+  filter_policy = "{\"Records\":{\"eventName\":[\"ObjectCreated:Copy\"]}}"
+  raw_message_delivery = true
 }
--- cloud/aws/Projects/event-based-architecture/sqs.tf
+++ cloud/aws/Projects/event-based-architecture/sqs.tf
@@ -1,6 +1,6 @@
-// Create SQS queue for notification messages from S3 
-resource "aws_sqs_queue" "sqs_queue_for_s3_updates" {
-  name                      = "queue_for_s3_updates"
+// Create SQS queue for "PUT" notification messages from S3 
+resource "aws_sqs_queue" "queue_for_processing_put_events_on_s3" {
+  name                      = "queue-for-processing-put-events-on-s3"
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
@@ -10,8 +10,8 @@ resource "aws_sqs_queue" "sqs_queue_for_s3_updates" {
 // SQS policy to allow 
 // - Allow SNS topic to push messages to the queue
 // -  
-resource "aws_sqs_queue_policy" "sqs_policy" {
-  queue_url =  aws_sqs_queue.sqs_queue_for_s3_updates.id
+resource "aws_sqs_queue_policy" "sqs_policy_put-events" {
+  queue_url = aws_sqs_queue.queue_for_processing_put_events_on_s3.id
 
   policy = <<EOF
 {
@@ -19,16 +19,89 @@ resource "aws_sqs_queue_policy" "sqs_policy" {
   "Id": "__default_policy",
   "Statement":  [
     {
-      "Sid": "StmtId_SQS",
+      "Sid": "sid1",
       "Effect": "Allow",
       "Principal": {
         "AWS": "*"
       },
       "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.sqs_queue_for_s3_updates.arn}",
+      "Resource": "${aws_sqs_queue.queue_for_processing_put_events_on_s3.arn}",
       "Condition": {
         "ArnLike": {
           "aws:SourceArn": "${aws_sns_topic.sns_topic_for_s3_upload_updates.arn}"
+        }
+      }
+    },
+    {
+      "Sid": "sid2",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": [
+        "sqs:ReceiveMessage",
+        "sqs:sendMessage"
+      ],
+      "Resource": "${aws_sqs_queue.queue_for_processing_put_events_on_s3.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_lambda_function.put_event_processor.arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+// Create SQS queue for "COPY" notification messages from S3 
+resource "aws_sqs_queue" "queue_for_processing_copy_events_on_s3" {
+  name                      = "queue-for-processing-copy-events-on-s3"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
+
+// SQS policy to allow 
+// - Allow SNS topic to push messages to the queue
+// -  
+resource "aws_sqs_queue_policy" "sqs_policy_copy-events" {
+  queue_url = aws_sqs_queue.queue_for_processing_copy_events_on_s3.id
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Id": "__default_policy",
+  "Statement":  [
+    {
+      "Sid": "sid1",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.queue_for_processing_copy_events_on_s3.arn}",
+      "Condition": {
+        "ArnLike": {
+          "aws:SourceArn": "${aws_sns_topic.sns_topic_for_s3_upload_updates.arn}"
+        }
+      }
+    },
+    {
+      "Sid": "sid2",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": [
+        "sqs:ReceiveMessage",
+        "sqs:sendMessage"
+      ],
+      "Resource": "${aws_sqs_queue.queue_for_processing_copy_events_on_s3.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_lambda_function.put_event_processor.arn}"
         }
       }
     }
