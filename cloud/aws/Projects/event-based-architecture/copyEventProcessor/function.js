6089404ff7e7a5467f8c3845085bc8902f970a3a
vikas4338 <vikas4338@gmail.com>
2023-12-08 10:44:47 -0600

    Updated lambda code to log information about event and s3 key

--- cloud/aws/Projects/event-based-architecture/copyEventProcessor/function.js
+++ cloud/aws/Projects/event-based-architecture/copyEventProcessor/function.js
@@ -1,4 +1,13 @@
 exports.handler = async (event) => {
     console.log('Event: ', event);
-    return "Hello World!! This function processes 'COPY' event notification.";
+    
+    let jsonBody = JSON.parse(event.Records[0].body);
+
+    let s3Object = jsonBody.Records[0].s3; 
+    
+    console.log('S3 Event: ' + jsonBody.Records[0].eventName)
+    console.log('S3 Bucket: ' + s3Object.bucket.name);
+    console.log('Object Key: ' + s3Object.object.key);
+    
+    return "Hello World!! This function processes COPY event notification.";
 };
